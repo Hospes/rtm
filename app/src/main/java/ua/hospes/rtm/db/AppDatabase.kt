564471187e45d7f2dbbd251443dff8425eff7ff7
Andrew Khloponin <hospes.a@gmail.com>
2019-09-15 00:20:34 +0300

    Enable exporting of database schema for better controlling entities/tables

--- app/src/main/java/ua/hospes/rtm/db/AppDatabase.kt
+++ app/src/main/java/ua/hospes/rtm/db/AppDatabase.kt
@@ -14,7 +14,8 @@ import ua.hospes.rtm.db.sessions.SessionEntity
 import ua.hospes.rtm.db.team.TeamDAO
 import ua.hospes.rtm.db.team.TeamEntity
 
-@Database(entities = [CarEntity::class, TeamEntity::class, DriverEntity::class, SessionEntity::class, RaceEntity::class], version = 1, exportSchema = false)
+@Database(version = 1, exportSchema = true,
+        entities = [CarEntity::class, TeamEntity::class, DriverEntity::class, SessionEntity::class, RaceEntity::class])
 @TypeConverters(Converters::class)
 internal abstract class AppDatabase : RoomDatabase() {
     abstract fun carDao(): CarDAO
